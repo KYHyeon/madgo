49fc037f7cc92441c9d1364472d0877362f4e2ac
yeonsoo_lim <yslim6168@naver.com>
2020-06-29 15:58:34 +0900

    toggle login_logout

--- public/javascripts/view/home.js
+++ public/javascripts/view/home.js
@@ -1,13 +1,32 @@
-document.getElementById('user1_login_btn').onclick = function(){
-    if(user = form_check_user1()){ //Every Form are checking
-        account_div = document.createElement('div');
-        user1_form = document.getElementById('user1_form');
-        account_div.innerHTML = `Hello! ${user}`;
-        account_div.setAttribute("id", "helloID");
-        document.getElementById('content1').replaceChild(account_div, user1_form);
-        
+document.getElementById('user1_btn').onclick = function(){
+    user1_btn = document.getElementById('user1_btn');
+    state = user1_btn.getAttribute("state");
+    user1_form = document.getElementById('user1_form');
+
+    if((user = form_check_user1()) && state == "Login"){ //Every Form are checking
+        state_Login(user);
+    }
+    else if(state == "Logout"){
+        state_Logout();
     }
 };
+
+function state_Login(user){
+    user1_btn.innerText = "Logout";
+    user1_btn.setAttribute("state","Logout");
+    account_div = document.createElement('div');
+    account_div.innerHTML = `Hello! ${user}`;
+    account_div.setAttribute("id", "helloID");
+    document.getElementById('user1_form').after(account_div);
+    document.getElementById('user1_form').style.display = "none";
+}
+function state_Logout(){
+    user1_btn.innerText = "Login";
+    user1_btn.setAttribute("state","Login");
+    user1_form.style.display = "block";
+    document.getElementById('helloID').remove();
+}
+
 
 function form_check_user1(){
     user1_id = document.getElementById('user1_id');
@@ -20,5 +39,6 @@ function form_check_user1(){
         alert(`Please fill on the PW`);
         return false;
     }
+    console.log(user1_id.value);
     return user1_id.value;
 }
