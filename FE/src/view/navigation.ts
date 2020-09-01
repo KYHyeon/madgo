759aa40b9a8d0a3218056be1aaa851ca1f7c9d59
yeonsoo_lim <yslim6168@naver.com>
2020-09-01 18:18:28 +0900

    200901_Navigation_Frame
    TODO :
    confirm the component of Navigation

--- FE/src/view/navigation.ts
+++ FE/src/view/navigation.ts
@@ -1,23 +1,15 @@
 let bodyShowAndHide = document.getElementById("body")?.style.display;
-let LRarrow = document.getElementById("head");
-let RLarrow = document.getElementById("body");
+let LRarrow = document.getElementById("nav_LR_arrow");
+let RLarrow = document.getElementById("nav_RL_arrow");
+let body = document.getElementById("body");
 LRarrow?.addEventListener('click', (event)=>{
-    RLarrow?.style.display = "flex";
+    body?.style.display = "flex";
     LRarrow?.style.display = "none";
-    RLarrow?.animate({
-        "width":["0%","100%"]
-    }, 500);
+    body?.animate({
+        "width":["30%","100%"]
+    }, 400);
 })
 RLarrow?.addEventListener('click', (event)=>{
     LRarrow?.style.display = "flex";
-    RLarrow?.style.display = "none";
-
-
+    body?.style.display = "none";
 })
-
-
-
-function showAndHide(state : string){
-    console.log(state);
-    return (state==="flex"?["none", "flex"]:["flex", "none"]);
-}
